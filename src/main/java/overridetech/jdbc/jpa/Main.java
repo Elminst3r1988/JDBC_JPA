c0e55d1cf32adfa22cda4ca34f14730d1205bb87
Konstantin <judge.konstantin@gmail.com>
2024-03-27 18:50:17 +0300

    test commit

--- src/main/java/overridetech/jdbc/jpa/Main.java
+++ src/main/java/overridetech/jdbc/jpa/Main.java
@@ -2,6 +2,6 @@ package overridetech.jdbc.jpa;
 
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        System.out.println("asd");
     }
 }
